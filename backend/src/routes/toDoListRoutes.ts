4e5420584de19bc08ad9127faed0a4f942dc3f2c
Miika Halmela <miika.halmela@gmail.com>
2025-07-20 22:17:26 +0300

    added routes to rest of the endpoints

--- backend/src/routes/toDoListRoutes.ts
+++ backend/src/routes/toDoListRoutes.ts
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.get('/lists', ToDoListController.getAllLists);
 router.get('/lists/:id', ToDoListController.getListById);
+router.post('/list', ToDoListController.createList);
+router.patch('/list/:id', ToDoListController.updateList);
+router.delete('/list/:id', ToDoListController.deleteList);
 
 export default router
