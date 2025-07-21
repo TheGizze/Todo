92bcf0932201c7b74fb808190abc6b247d6df0a8
Miika Halmela <miika.halmela@gmail.com>
2025-07-21 21:27:31 +0300

    added error response for invalid item content

--- backend/src/responses/errorResponses.ts
+++ backend/src/responses/errorResponses.ts
@@ -3,5 +3,6 @@ export const errors = {
   listNotFound: (id: string) => ({message: `No list found with id: ${id}`}),
   itemNotFound: (id: string) => ({message: `No item found with id: ${id}`}),
   invalidListName: (validationErrors: string[]) => ({message: 'invalid name', violations: validationErrors}),
+  invalidItemContent: (validationErrors: string[]) => ({message: 'invalid content', violations: validationErrors}),
   missingValues: (values: string[]) => ({message: 'request body missing values', missingValues: values})
 };
