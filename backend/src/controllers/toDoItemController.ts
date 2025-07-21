d4db65ddcc55d46e17277af278ffac5912ae601e
Miika Halmela <miika.halmela@gmail.com>
2025-07-21 21:58:40 +0300

    added getlist items and get listItem to controller

--- backend/src/controllers/toDoItemController.ts
+++ backend/src/controllers/toDoItemController.ts
@@ -9,8 +9,24 @@ export const createItem = (_req: Request, res: Response) => {
     const validationErrors = validator.validateString(_req.body.content);
     if (validationErrors.length > 0) return res.status(400).json(errors.invalidItemContent(validationErrors));
     
-    const newItem = ToDoService.createListItem(_req.params.id, _req.body.content);
+    const newItem = ToDoService.createListItem(_req.params.listId, _req.body.content);
     if (!newItem) return res.status(404).json(errors.listNotFound(_req.params.id));
 
     return res.status(200).json(newItem);
+};
+
+export const getListItems = (_req: Request, res: Response) => {
+    const items = ToDoService.getListItems(_req.params.ListId);
+
+    if (!items) return res.status(404).json(errors.listNotFound);
+
+    return res.status(200).json(items);
+};
+
+export const getListItem = (_req: Request, res: Response) => {
+    const item = ToDoService.getListItem(_req.params.listId, _req.params.itemId);
+
+    if(!item) return res.status(404).json(errors.listOrItemNotFound(_req.params.listId, _req.params.itemId));
+
+    return res.status(200).json(item);
 };
