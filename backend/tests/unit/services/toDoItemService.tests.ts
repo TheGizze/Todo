712f08e4536a29b9461000b36d6832ba70d3d303
Miika Halmela <miika.halmela@gmail.com>
2025-07-21 16:21:17 +0300

    created base for toDoItemService unit tests

---
+++ backend/tests/unit/services/toDoItemService.tests.ts
@@ -0,0 +1,41 @@
+import * as service from '../../../src/services/toDoItemService'
+import { toDoLists } from '../../../src/mockDb'
+import { ToDoItem } from '../../../src/models/ToDoItem'
+
+
+describe('ToDoItemService', () => {
+
+    //Create Item
+    describe('Create new item', () => {
+
+    });
+
+    //Read Items
+    describe('Get all Items', () => {
+        
+    });
+    
+    //Read Item
+    describe('Get item by id', () => {
+        
+    });
+
+
+    //Update Item
+    describe('Update item by id', () => {
+        
+    });
+
+    //Delete Items
+    describe('Delete All Items from list', () => {
+        
+    });
+    // Delete Item
+    describe('delete item by id', () => {
+        
+    });
+
+
+
+
+});
