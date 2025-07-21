925cf28d8c16b5914a06297e94d82c14dda150f9
Miika Halmela <miika.halmela@gmail.com>
2025-07-21 18:22:42 +0300

    fixed unit tests and mockDb to use nanoid and reconfigred jest.config to handle es modules

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -5,9 +5,16 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  preset: "ts-jest",
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    ...tsJestTransformCfg,
+    '^.+\\.ts$': ['ts-jest', { 
+      useESM: true 
+    }]
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(nanoid)/)'
+  ],
   testMatch: [
     "**/tests/**/*.ts",
     "**/?(*.)+(spec|test).ts"
