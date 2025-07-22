0f305c77dd5e364509da1bcb4a7b1e070a9a9e8b
Miika Halmela <miika.halmela@gmail.com>
2025-07-22 19:45:34 +0300

    added error handler to API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import toDoListRoutes from './routes/toDoListRoutes';
+import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', toDoListRoutes);
+app.use(errorHandler);
 
 if (require.main === module) {
   app.listen(port, () => console.log(`Server running on port ${port}`));
