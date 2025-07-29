6873115c485e96182d1d1c670bdf997576153ca5
Miika Halmela <miika.halmela@gmail.com>
2025-07-29 22:22:12 +0300

    added missingValues field for missing values error

--- backend/src/errors/requestValidationErrors.ts
+++ backend/src/errors/requestValidationErrors.ts
@@ -1,10 +1,13 @@
 
 export class MissingValuesError extends Error {
     status: number;
+    missingValues: string[]
 
-    constructor(message: string){
+    constructor(message: string, missingValues: string[]){
         super(message);
+        this.missingValues = missingValues;
         this.name = 'MissingValuesError';
         this.status = 400
     }
 }
+// {message: 'request body missing values', missingValues: values})
