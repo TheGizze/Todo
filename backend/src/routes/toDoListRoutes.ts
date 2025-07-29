511bb3f151344528e59ea34c316ebd95bafba20b
Miika Halmela <miika.halmela@gmail.com>
2025-07-29 22:23:54 +0300

    added request validation for list creation

--- backend/src/routes/toDoListRoutes.ts
+++ backend/src/routes/toDoListRoutes.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import * as ToDoListController from '../controllers/toDoListController'
 import * as ToDoItemController from '../controllers/toDoItemController'
+import { validateRequest } from '../middleware/validators/requestValidation';
+import * as listSchemas from '../schemas/requests/listSchemas';
 
 const router = express.Router();
 
 // List routes
 router.get('/lists', ToDoListController.getAllLists);
 router.get('/lists/:listId', ToDoListController.getList);
-router.post('/lists', ToDoListController.createList);
+router.post('/lists', validateRequest(listSchemas.createListSchema), ToDoListController.createList);
 router.patch('/lists/:listId', ToDoListController.updateList);
 router.delete('/lists/:listId', ToDoListController.deleteList);
 
