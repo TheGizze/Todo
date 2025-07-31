cc346852e36d74ff5c88975ff508505adb86b869
Miika Halmela <miika.halmela@gmail.com>
2025-07-31 11:39:31 +0300

    added validation function for updating item

--- backend/src/middleware/validators/requestValidation.ts
+++ backend/src/middleware/validators/requestValidation.ts
@@ -5,7 +5,7 @@ import { MissingValuesError } from '../../errors/requestValidationErrors';
 export const validateRequest = (schema: z.ZodType) => {
     return (req: Request, _res: Response, next: NextFunction) => {
         const result = schema.safeParse(req.body);
-
+        console.info("post was called");
         if(!result.success){
             const issues = result.error.issues.map(issue => issue.message);
 
@@ -13,6 +13,19 @@ export const validateRequest = (schema: z.ZodType) => {
         }
         req.body = result.data;
         
+        next();
+    };
+};
+
+export const validateUpdateItemRequest = (schema: z.ZodType) => {
+    return (req: Request, _res: Response, next: NextFunction) => {
+        const result = schema.safeParse(req.body);
+        console.info("patch was called");
+        if(!result.success){
+            const issues = result.error.issues.map(issue => issue.message);
+            throw new MissingValuesError('At least One field required', issues);
+        }
+        req.body = result.data;
         next();
     };
 };
