99357c42ae03a9dcbbd95f83f7006a5401847c61
Miika Halmela <miika.halmela@gmail.com>
2025-07-31 11:38:41 +0300

    added validation for business logic

---
+++ backend/src/middleware/validators/businessValidation.ts
@@ -0,0 +1,22 @@
+
+import { z } from 'zod';
+import { InvalidListNameError } from '../../errors/dataValidationErrors';
+import { InvalidItemContentError } from '../../errors/dataValidationErrors';
+import { ToDoItem } from '../../models/ToDoItem';
+
+export const validateString = (str: string, schema: z.ZodType) => {
+    const result = schema.safeParse(str)  
+    if(!result.success){
+        const issues = result.error.issues.map(issue => issue.message);
+        throw new InvalidListNameError('invalid content in title', issues);
+    }
+}
+
+export const validateItem = (item: Partial <ToDoItem>, schema: z.ZodObject) =>{
+    const result = schema.safeParse(item);
+    if(!result.success){
+        const fieldErrors = z.flattenError(result.error).fieldErrors;
+        console.info(fieldErrors);
+        throw new InvalidItemContentError('invalid item values', fieldErrors);
+    }
+}
