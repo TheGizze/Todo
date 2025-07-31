b96103ce773a0b8cebe02cf833f03a6259646ca9
Miika Halmela <miika.halmela@gmail.com>
2025-07-31 11:30:53 +0300

    removed all the validation and moved it to service layer, removed unused imports and removed under score from _req sense it's being used

--- backend/src/controllers/toDoItemController.ts
+++ backend/src/controllers/toDoItemController.ts
@@ -1,25 +1,14 @@
 import { Request, Response } from "express";
 import * as ToDoService from '../services/toDoItemService'
-import { errors } from '../errors/errorResponses'
-import * as validator from '../middleware/validators/common'
-import { ToDoItem } from "../models/ToDoItem";
 
-export const createItem = (_req: Request, res: Response) => {
-    //create request validator that handles this kind of stuff later
-    if (_req.body.content === undefined) return res.status(400).json(errors.missingValues(["content"]));
-    //---------------------------------------------------
 
-    //move this logic to a data validator and validate it on a service level
-    const validationErrors = validator.validateString(_req.body.content);
-    if (validationErrors.length > 0) return res.status(400).json(errors.invalidItemContent(validationErrors));
-    //---------------------------------------------------
-    const newItem = ToDoService.createListItem(_req.params.listId, _req.body.content);
-
+export const createItem = (req: Request, res: Response) => {
+    const newItem = ToDoService.createListItem(req.params.listId, req.body);
     return res.status(200).json(newItem);
 };
 
-export const getListItems = (_req: Request, res: Response) => {
-    const items = ToDoService.getListItems(_req.params.listId);
+export const getListItems = (req: Request, res: Response) => {
+    const items = ToDoService.getListItems(req.params.listId);
     return res.status(200).json(items);
 };
 
@@ -28,21 +17,12 @@ export const getListItem = (_req: Request, res: Response) => {
     return res.status(200).json(item);
 };
 
-export const updateListItem = (_req: Request, res: Response) => {
-    //create request validator that handles this kind of stuff later
-    const missingValues: string[] = [];
-    const updates: Partial<ToDoItem> = {};
-    if (_req.body.completed === undefined) { missingValues.push("completed") } else { updates.completed = _req.body.completed };
-    if (_req.body.content === undefined) { missingValues.push("content") } else { updates.content = _req.body.content };
-    if (missingValues.length > 0) return res.status(400).json(errors.missingValues(missingValues));
-    //---------------------------------------------------
-
-    const updatedItem = ToDoService.updateListItem(_req.params.listId, _req.params.itemId, updates);
-
+export const updateListItem = (req: Request, res: Response) => {
+    const updatedItem = ToDoService.updateListItem(req.params.listId, req.params.itemId, req.body);
     return res.status(200).json(updatedItem);
 };
 
-export const deleteListItem = (_req: Request, res: Response) => {
-    const deletedItem = ToDoService.deleteItem(_req.params.listId, _req.params.itemId);
+export const deleteListItem = (req: Request, res: Response) => {
+    const deletedItem = ToDoService.deleteItem(req.params.listId, req.params.itemId);
     return res.status(200).json(deletedItem);
 };
