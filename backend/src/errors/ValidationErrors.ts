a79c6c367afe1a1fb802b5ec3f6f3aef909aac5b
Miika Halmela <miika.halmela@gmail.com>
2025-08-01 13:04:59 +0300

    refactored baseValidationError to handle calling of the errorParser to reformat errors

--- backend/src/errors/ValidationErrors.ts
+++ backend/src/errors/ValidationErrors.ts
@@ -1,11 +1,19 @@
+import { z } from 'zod';
+import { parseFieldErrors } from '../middleware/errorHandling/errorParser';
 abstract class BaseValidationError extends Error {
     abstract status: number;
-    abstract violations: Record <string, string[]>;
+    violations: Record<string, string[]>;
 
-    constructor(message: string){
+    constructor(message: string, errors: Record <string, string[]> | z.ZodError<any>){
         super(message);
         this.name = this.constructor.name;
         
+        if (errors instanceof z.ZodError){
+            const fieldErrors = z.flattenError(errors).fieldErrors;
+            this.violations = parseFieldErrors(fieldErrors);
+        }else{
+            this.violations = errors;
+        }
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, this.constructor);
         }
@@ -14,20 +22,16 @@ abstract class BaseValidationError extends Error {
 
 export class DataValidationError extends BaseValidationError {
     status = 400;
-    violations: Record<string, string[]>;
 
-    constructor(violations: Record<string, string[]>){
-        super('Data validation failed');
-        this.violations = violations;
+    constructor(violations: Record<string, string[]> | z.ZodError<any>){
+        super('Data validation failed', violations);
     }
 }
 
 export class RequestValidationError extends BaseValidationError {
     status = 400;
-    violations: Record<string, string[]>;
 
-    constructor(violations: Record<string, string[]>){
-        super('Request validation failed');
-        this.violations = violations;
+    constructor(violations: Record<string, string[]> | z.ZodError<any>){
+        super('Request validation failed', violations);
     }
 }
