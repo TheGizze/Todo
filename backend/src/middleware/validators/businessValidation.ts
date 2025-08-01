c7a0d2da98899f54016c1d3ea907acbe21a75a32
Miika Halmela <miika.halmela@gmail.com>
2025-08-01 13:06:42 +0300

    refoctored validators to take advantage of the error parsing in the error

--- backend/src/middleware/validators/businessValidation.ts
+++ backend/src/middleware/validators/businessValidation.ts
@@ -1,13 +1,10 @@
 import { z } from 'zod';
 import { DataValidationError} from '../../errors/ValidationErrors';
-import { parseFieldErrors } from '../errorHandling/errorParser';
 
 export const validateData = <T extends z.ZodRawShape>(item: Partial<z.infer<z.ZodObject<T>>>, schema: z.ZodObject<T>) =>{
     const result = schema.safeParse(item);
-    if(!result.success){
-        const fieldErrors  = z.flattenError(result.error).fieldErrors;
-        const cleanedFieldErrors = parseFieldErrors(fieldErrors);
-        throw new DataValidationError(cleanedFieldErrors);
+    if(!result.success){;
+        throw new DataValidationError(result.error);
     }
     return result.data;
 }
--- backend/src/middleware/validators/requestValidation.ts
+++ backend/src/middleware/validators/requestValidation.ts
@@ -1,16 +1,12 @@
 import { z } from 'zod';
 import { Request, Response, NextFunction } from 'express';
 import { RequestValidationError } from '../../errors/ValidationErrors';
-import { parseFieldErrors } from '../errorHandling/errorParser';
 
 export const validateRequest = (schema: z.ZodType) => {
     return (req: Request, _res: Response, next: NextFunction) => {
         const result = schema.safeParse(req.body);
         if(!result.success){
-            console.info(result.error);
-            const fieldErrors  = z.flattenError(result.error).fieldErrors;
-            const cleanedFieldErrors = parseFieldErrors(fieldErrors);
-            throw new RequestValidationError(cleanedFieldErrors);
+            throw new RequestValidationError(result.error);
         }
         req.body = result.data;
         
