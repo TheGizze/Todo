7e19a5cbdfd7cbbaa8309d9e37c8e9dc0303fb04
Miika Halmela <miika.halmela@gmail.com>
2025-08-03 12:39:27 +0300

    added logging implementations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,20 @@
+import 'dotenv/config'
 import express from 'express';
 import toDoListRoutes from './routes/toDoListRoutes';
 import { errorHandler } from './middleware/errorHandling/errorHandler';
+import { logger } from './utils/logger/logger';
+import { requestLogger } from './middleware/logging/requestLogger';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(requestLogger);
 app.use(express.json());
 app.use('/api', toDoListRoutes);
 app.use(errorHandler);
 
 if (require.main === module) {
-  app.listen(port, () => console.log(`Server running on port ${port}`));
+  app.listen(port, () => logger.info({ port }, 'Server started successfully'));
 }
 
 export default app;
