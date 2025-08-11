a4a6e7d99d60ac27bc62ce740fad1fbe2121a4f8
Miika Halmela <miika.halmela@gmail.com>
2025-08-11 17:13:28 +0300

    made controllers async

--- backend/src/controllers/toDoItemController.ts
+++ backend/src/controllers/toDoItemController.ts
@@ -2,27 +2,27 @@ import { Request, Response } from "express";
 import * as ToDoService from '../services/toDoItemService'
 
 
-export const createItem = (req: Request, res: Response) => {
-    const newItem = ToDoService.createListItem(req.params.listId, req.body);
+export const createItem = async(req: Request, res: Response) => {
+    const newItem = await ToDoService.createListItem(req.params.listId, req.body);
     return res.status(200).json(newItem);
 };
 
-export const getListItems = (req: Request, res: Response) => {
-    const items = ToDoService.getListItems(req.params.listId);
+export const getListItems = async(req: Request, res: Response) => {
+    const items = await ToDoService.getListItems(req.params.listId);
     return res.status(200).json(items);
 };
 
-export const getListItem = (_req: Request, res: Response) => {
-    const item = ToDoService.getListItem(_req.params.listId, _req.params.itemId);
+export const getListItem = async(_req: Request, res: Response) => {
+    const item = await ToDoService.getListItem(_req.params.listId, _req.params.itemId);
     return res.status(200).json(item);
 };
 
-export const updateListItem = (req: Request, res: Response) => {
-    const updatedItem = ToDoService.updateListItem(req.params.listId, req.params.itemId, req.body);
+export const updateListItem = async(req: Request, res: Response) => {
+    const updatedItem = await ToDoService.updateListItem(req.params.listId, req.params.itemId, req.body);
     return res.status(200).json(updatedItem);
 };
 
-export const deleteListItem = (req: Request, res: Response) => {
-    const deletedItem = ToDoService.deleteListItem(req.params.listId, req.params.itemId);
+export const deleteListItem = async(req: Request, res: Response) => {
+    const deletedItem = await ToDoService.deleteListItem(req.params.listId, req.params.itemId);
     return res.status(200).json(deletedItem);
 };
